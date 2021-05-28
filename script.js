325ef7c490f2d02e209532b265d52f2d59db3caf
timotiousPrime <timodb031@gmail.com>
2021-05-28 21:44:02 +0200

    created some basic functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(x, y){
+    return x + y
+}
+
+function subtract(x, y) {
+    return x - y
+}
+
+function multiply(x, y) {
+    return x * y
+}
+
+function divide(x, y) {
+    if (y === 0) return 'The second value has to be greater than your IQ. It can\'t be zero' 
+    else {
+        return x / y
+    }
+}
+
+function percentOf(x, y, z) {
+    return x z (y * (x / 100))}
