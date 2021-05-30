7745953bff4d0dd47500122741f6a79a893d3d0b
timotiousPrime <timodb031@gmail.com>
2021-05-30 22:11:12 +0200

    added num content to screen

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
 // Functions
 
 function add(x, y){
-    return x + y
-}
+    return x + y;
+};
 
 function subtract(x, y) {
-    return x - y
-}
+    return x - y;
+};
 
 function multiply(x, y) {
-    return x * y
-}
+    return x * y;
+};
 
 function divide(x, y) {
     if (y === 0) return 'The second value has to be greater than your IQ. It can\'t be zero' 
     else {
-        return x / y
-    }
-}
+        return x / y;
+    };
+};
 
 function pi() {
-    return 22/7
-}
+    return 22/7;
+};
 
 function percentOf(x, y) {
     let percent = x * (y / 100);
 
-    return percent
-}
+    return percent;
+};
 
 function squared(x) {
-    return x * x
-}
+    return x * x;
+};
 
 function squareRoot(x) {
     if (x >= 0) return Math.sqrt(x)
     else return 'You can\'t get the square root of your IQ. It can\'t be less than zero' 
-}
+};
 
 function factoral(x) {
-    let array = []
+    let array = [];
     for (let i = 1; i <= x; i++ ) {
-        array.push(i)
-    }
+        array.push(i);
+    };
 
     let result =array.reduce( (acc, num) => acc * num)
-    return result
-}
+    return result;
+};
 
 //
+
+let inputPair = [];
+
+let inputNum = 0;
+
+let screenDisplay = document.getElementById('screen');
+
+let numBtns = document.querySelectorAll('.num');
+numBtns.forEach(numBtn => numBtn.addEventListener('click', logEvent))
+
+function logEvent(e) {
+    inputNum += (e.target.textContent);
+    num = parseFloat(inputNum)
+    console.log(num) 
+    screenDisplay.textContent = num
+
+    
+}
